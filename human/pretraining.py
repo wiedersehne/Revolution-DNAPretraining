8c62b0d9d890954fb9e2cca084d4f7822510874a
Tong Yu <wiedersehne@gmail.com>
2023-01-17 12:55:20 +0100

    Update pretraining.py

--- human/pretraining.py
+++ human/pretraining.py
@@ -1,13 +1,12 @@
-print("Inside python")
 from random import randint, shuffle
 from random import random as rand
 from utils import *
-from classify import classify
 import pandas as pd
 import torch.nn as nn
 import sys
-#import chord_mixer as models
-import models
+import yaml
+import argparse
+import revolution_models
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from datetime import datetime
@@ -24,61 +23,35 @@ from Bio import SeqIO
 from sklearn.preprocessing import LabelBinarizer, LabelEncoder
 from sklearn.metrics import roc_auc_score, average_precision_score
 import wandb
-from model_former_ende import *
-print("initializing wandb")
-wandb.init(settings=wandb.Settings(start_method='fork'), project="Xformer", entity="tonyu")
-print("wandb initialized")
+from revolution_models import *
+wandb.init(settings=wandb.Settings(start_method='fork'), project="Pretraining", entity="tonyu")
 torch.backends.cudnn.bechmark = True
 
-#393216
-pretrain_cfg = {
-    "seed": 3431,
-    "dim": 5,
-    "hdim1":16,
-    "hdim2":8,
-    "kernel_size": 3,
-    "n_layers": 9,
-    "max_len": 1000,
-    "batch_size": 64,
-    "mask_ratio": 0.3,
-    "real_mask": 0.8,
-    "lr": 0.003,
-    "n_epochs": 50,
-    "train_num":40000,
-    "n_cores": mp.cpu_count(),
-    "device": "cuda"
-}
-
-classify_cfg = {
-    "dim": 5,
-    "hdim1":16,
-    "hdim2":8,
-    "kernel_size": 3,
-    "n_layers": 9,
-    "max_len": 1000,
-    "n_class": 49,
-    "batch_size": 8,
-    "lr": 0.00003,
-    "n_epochs": 50,
-    "device": "cuda"
-}
 
 class Dict2Class:
     def __init__(self, dictionary):
         for k, v in dictionary.items():
             setattr(self, k, v)
 
-print(pretrain_cfg)
-print(classify_cfg)
+
+parser = argparse.ArgumentParser(description="experiments")
+parser.add_argument("--problem", type=str, default='Pretraining')
+parser.add_argument("--model", type=str, default='Revolution')
+args = parser.parse_args()
+
+# Parsing training config
+stream = open("config.yaml", 'r')
+cfg_yaml = yaml.safe_load(stream)
+pretrain_cfg = cfg_yaml["Pretraining"][args.model]
 pretrain_cfg = Dict2Class(pretrain_cfg)
-classify_cfg = Dict2Class(classify_cfg)
+pretrain_cfg.n_cores = mp.cpu_count()
+print('pretraining config', pretrain_cfg)
 
 
 class DatasetCreator(Dataset):
     """
     Class to construct a dataset for training/inference
     """
-
     def __init__(self, gene_labels, masked_gene, masked_pos, sequences_num, ids, pos):
         self.data = gene_labels
         self.masked_data = masked_gene
@@ -92,13 +65,8 @@ class DatasetCreator(Dataset):
         Returns: tuple (sequence, masked_tokens, masked_position)
         """
         randid = random.randint(0, 78333)
-        # print("index",index)
-        # print(self.ids[index][0:2])
-        # print(self.ids[index][3:])
         random_chrom = int(self.ids[randid][3:])
         masked_gene, gene_labels, masked_vec = self.masked_data[random_chrom], self.data[random_chrom], self.masked_pos[random_chrom]
-        # length = len(gene_labels)
-        # cursor = random.randint(0, length-pretrain_cfg.max_len)
         cursor = self.pos[randid]
         halflength = int(pretrain_cfg.max_len/2)
         if cursor + halflength >= len(gene_labels):
@@ -114,14 +82,13 @@ class DatasetCreator(Dataset):
 
 
 class UniformMasking():
-    """ Pre-processing steps for pretraining transformer """
+    """ Pre-processing steps for pretraining revolution """
     def __init__(self, mask_prob, real_mask_ratio):
         super().__init__()
         self.mask_ratio = mask_prob
         self.real_mask_ratio = real_mask_ratio
 
     def __call__(self, instance):
-        # print(instance[0:10])
         # 1. get binary-encoded masked indexes and masked positions
         # random_masked_ratio = (1 - self.real_mask_ratio) / 2
         uniform_vec = np.random.rand(len(instance))
@@ -147,35 +114,31 @@ class UniformMasking():
 
 
 # Model for Pretraining
-# class Model4Pretrain(nn.Module):
-#     "CDIL Model for Pretrain : Masked LM"
-#     def __init__(self, cfg):
-#         super().__init__()
-#         self.cdilNet = models.CDIL_Conv(cfg.dim, [cfg.hdim1]*cfg.n_layers, cfg.hdim1*2, cfg.kernel_size)
-#         # self.cdilNet2 = models.CDIL_Conv(cfg.hdim1, [cfg.hdim1]*cfg.n_layers, cfg.hdim1, cfg.kernel_size)
-#         # self.cdilNet = models.ChordMixerNet(cfg.dim, 12, cfg.max_len, [128, "GELU"], 0)
-#         self.hidden_list = [cfg.hdim2]*cfg.n_layers
-#         self.hidden_list[-1] = cfg.dim
-#         self.decoder = models.CDIL_Conv(cfg.hdim1, self.hidden_list, cfg.hdim2*2, cfg.kernel_size)
+class Model4Pretrain(nn.Module):
+    "CDIL Model for Pretrain : Masked LM"
+    def __init__(self, cfg):
+        super().__init__()
+        self.encoder = RevolutionLayer(cfg.dim, [cfg.hdim1]*cfg.n_layers, cfg.hdim1*2, cfg.kernel_size)
+        self.hidden_list = [cfg.hdim2]*cfg.n_layers
+        self.hidden_list[-1] = cfg.dim
+        self.decoder = RevolutionLayer(cfg.hdim1, self.hidden_list, cfg.hdim2*2, cfg.kernel_size)
 
-#     def forward(self, input_seq):
-#         input_seq = input_seq.float()
-#         # encoder
-#         h = torch.permute(input_seq, (0, 2, 1))
-#         h = self.cdilNet(h)
-#         # decoder
-#         h = self.decoder(h)
-#         h = torch.permute(h, (0, 2, 1))
+    def forward(self, input_seq):
+        input_seq = input_seq.float()
+        # encoder
+        h = torch.permute(input_seq, (0, 2, 1))
+        h = self.encoder(h)
+        # decoder
+        h = self.decoder(h)
+        h = torch.permute(h, (0, 2, 1))
 
-#         return h
+        return h
 
 
 def masking_one_chrom(gene_id):
     print("started ")
     gene = list(hg38_dict[gene_id].lower())
-    # gene = [i for i in gene if i != 'n']
     gene_label = le.transform(copy.deepcopy(gene))
-    # gene_label = lb.transform(copy.deepcopy(gene))
     masking = UniformMasking(pretrain_cfg.mask_ratio, pretrain_cfg.real_mask)
     masked_gene, masked_vec = masking(np.array(lb.transform(gene)))
     print("done")
@@ -208,14 +171,11 @@ if __name__ == '__main__':
     lb = LabelBinarizer()
     le = LabelEncoder()
     print("preprocessing-------")
-    gene_label, maksed_gene, masked_pos = parallel_process(lb, le)#"chr19_KI270938v1_alt")
-    # for x in maksed_gene:
-    #     print(len(x))
+    gene_label, maksed_gene, masked_pos = parallel_process(lb, le)
     train_df = pd.read_csv('./data/vcf_train.csv')
     ids = train_df.chr.values
     pos = train_df.pos.values
     print(len(ids))
-    print(ids[0:50])
     print("building dataloader-------")
     # 2. build data loader
     train_data_iter = DataLoader(
@@ -249,14 +209,9 @@ if __name__ == '__main__':
 
     print("training----------")
     model = Model4Pretrain(pretrain_cfg)
-    # model = Model4Pretrain("nystromer", 2, 2, 5, 16, 8)
     print(model)
-    # #criterion = nn.BCEWithLogitsLoss()
     criterion = nn.CrossEntropyLoss(reduce=False)
-    # criterion = nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr = pretrain_cfg.lr)
     trainer = train.Trainer(pretrain_cfg, model, train_data_iter, valid_data_iter, test_data_iter,
                             optimizer, criterion, save_dir, device)
     trainer.train(data_parallel)
-
-    classify(classify_cfg, pretrain_cfg)
